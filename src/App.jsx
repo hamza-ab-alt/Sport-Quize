423345e7b5f69a2c7af2e95b667d70f5259c6334
kaoutar <kaoutar@gmail.com>
2026-03-17 09:40:23 +0000

    validation d'input du formulaire

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [number, setNumber] = useState("");
+  const [message, setMessage] = useState("");
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setNumber(value);
+
+    if (value === "") {
+      setMessage(""); 
+    } else if (Number(value) > 0) {
+      setMessage(" Number is valid");
+    } else {
+      setMessage("Please enter a number greater than 0");
+    }
+  }
+
   return (
     <>
-    <div className="container">
-      <h2>Pick Your Questions and Go!</h2>
-      <form>
+      <div className="container">
+        <h2>Pick Your Questions and Go!</h2>
+        <form>
           <label>Number of Questions</label>
-    <input type="number" />
+          <input
+            value={number}
+            onChange={handleChange}
+            type="number"
+            placeholder="Enter a number"
+          />
+          <p className="message">{message}</p>
 
-    <label>Difficulty</label>
-    <select>
-      <option>Easy</option>
-      <option>Medium</option>
-      <option>Hard</option>
-    </select>
+          <label>Difficulty</label>
+          <select>
+            <option>Easy</option>
+            <option>Medium</option>
+            <option>Hard</option>
+          </select>
 
-    <button>Start Quiz</button>
-      </form>
+          <button>Start Quiz</button>
+        </form>
       </div>
     </>
   );
